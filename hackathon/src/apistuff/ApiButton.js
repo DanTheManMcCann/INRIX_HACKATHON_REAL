a1054bbe1a74e0e70c0a8cd98791c02c5ab770bb
Daniel MS <danielmccannsayles@gmail.com>
2021-11-13 17:36:55 -0800

    Update ApiButton.js

--- hackathon/src/apistuff/ApiButton.js
+++ hackathon/src/apistuff/ApiButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AST_SymbolExport } from 'terser';
 //This is API Folder
 
 async function tokenrequest(){
@@ -8,47 +9,48 @@ async function tokenrequest(){
     return Token1
 }
 
-async function geoCode(){
-
-    const response = await fetch("https://maps.googleapis.com/maps/api/geocode/json?address=24%20Sussex%20Drive%20Ottawa%20ON&key=AIzaSyCrxWO6tk28BtEckg7dG9XwvBpwWliynsU");
+async function geoCode(addressGiven){
+    if (addressGiven){
+    const addressCrazy = encodeURIComponent(addressGiven.trim());
+    const response = await fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + addressCrazy + "&key=AIzaSyCrxWO6tk28BtEckg7dG9XwvBpwWliynsU");
     const json = await response.json();
     const Address=json;
     console.log(Address);
     return Address;
+    }
 }
-
-
-
-
-
 
 
 function ApiButton() {
     const [input, setInput] = React.useState('');
+    const [input2, setInput2] = React.useState('');
+    const addressGiven=input;
+    const addressGivenTwo=input2;
 
-    async function handleClick(input){
+    async function handleClick(){
 
         //get api token for inrix
         const token1= await tokenrequest(); //have to use a fuckton of awaits
         // console.log(token1);
     
-        const address = await geoCode();
-        const address2=address.results['0'].geometry.location;
-        console.log(address2.lat);
-        console.log(address2.lng);
-        // for (const variable in address2){
-        //     console.log(variable);
-        //     console.log("SPace");
-        // }
-        console.log(input)
-        
+        const address = await geoCode(addressGiven);
+        const addressbrokendown=address.results['0'].geometry.location;
+        console.log(addressbrokendown.lat);
+        console.log(addressbrokendown.lng);
+
+        const address2 = await geoCode(addressGivenTwo);
+        const address2brokendown=address2.results['0'].geometry.location;
+        console.log(address2brokendown.lat);
+        console.log(address2brokendown.lng);
     
     }   
 
   return (
       <>
-      <input value={input} onInput={e => setInput(e.target.value)}/>
-      <button onClick={handleClick}>Hello </button>
+        <input placeholder="destination" value={input} onInput={e => setInput(e.target.value)}/>
+        <input placeholder="current location" value={input2} onInput={e => setInput2(e.target.value)}/>
+        <button onClick={handleClick}>Route </button>
+        <p>{input2}</p>
       </>
   );
 }
