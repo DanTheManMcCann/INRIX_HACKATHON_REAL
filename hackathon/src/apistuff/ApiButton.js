a8fe2d8df00ce4b9951798c8178cd091778cf90c
Daniel MS <danielmccannsayles@gmail.com>
2021-11-13 16:54:05 -0800

    Update ApiButton.js

--- hackathon/src/apistuff/ApiButton.js
+++ hackathon/src/apistuff/ApiButton.js
@@ -1,38 +1,55 @@
 import React from 'react';
 //This is API Folder
 
-function handleClick(){
+async function tokenrequest(){
+    const response = await fetch("https://api.iq.inrix.com/auth/v1/appToken?appId=lrzpgru190&hashToken=bHJ6cGdydTE5MHxiT1I3MGY2cmVONW1Dd2k4SmlIcjA1dXd4d2ZSTWlmdTMxd3pUb05M");
+    const json = await response.json();
+    const Token1=json.result.token;
+    return Token1
+}
 
-    let accessToken;
+async function geoCode(){
 
-    const credentials = { appId: 'lrzpgru190', hashToken: 'bHJ6cGdydTE5MHxiT1I3MGY2cmVONW1Dd2k4SmlIcjA1dXd4d2ZSTWlmdTMxd3pUb05M' };
-
-
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-
-    async function getAccessToken(appId, hashToken) {
-        let response = await fetch("https://api.iq.inrix.com/auth/v1/appToken?appId=lrzpgru190&hashToken=bHJ6cGdydTE5MHxiT1I3MGY2cmVONW1Dd2k4SmlIcjA1dXd4d2ZSTWlmdTMxd3pUb05M", requestOptions)
-        let json;
-        if (response.ok){
-            json= await response.json();
-            console.log(json);
-        }
-
-        console.log(json.result.token);
-
-    }
-
-    accessToken = getAccessToken( credentials.appId, credentials.hashToken);
+    const response = await fetch("https://maps.googleapis.com/maps/api/geocode/json?address=24%20Sussex%20Drive%20Ottawa%20ON&key=AIzaSyCrxWO6tk28BtEckg7dG9XwvBpwWliynsU");
+    const json = await response.json();
+    const Address=json;
+    console.log(Address);
+    return Address;
 }
 
 
+
+
+
+
+
 function ApiButton() {
+    const [input, setInput] = React.useState('');
+
+    async function handleClick(input){
+
+        //get api token for inrix
+        const token1= await tokenrequest(); //have to use a fuckton of awaits
+        // console.log(token1);
+    
+        const address = await geoCode();
+        const address2=address.results['0'].geometry.location;
+        console.log(address2.lat);
+        console.log(address2.lng);
+        // for (const variable in address2){
+        //     console.log(variable);
+        //     console.log("SPace");
+        // }
+        console.log(input)
+        
+    
+    }   
 
   return (
+      <>
+      <input value={input} onInput={e => setInput(e.target.value)}/>
       <button onClick={handleClick}>Hello </button>
+      </>
   );
 }
 
